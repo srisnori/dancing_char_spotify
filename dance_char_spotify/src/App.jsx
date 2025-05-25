d9149a99f8023dd060ccc0ca89b744c1bc4c9cfc
srisnori <srisnori@gmail.com>
2025-05-24 23:18:18 -0700

    login

--- dance_char_spotify/src/App.jsx
+++ dance_char_spotify/src/App.jsx
@@ -1,14 +1,24 @@
-import { redirectToSpotifyAuth } from "./utils/spotify";
-import DancingCharacter from "./components/DancingCharacter";
+import React from 'react';
+
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+const RESPONSE_TYPE = 'token';
 
 function App() {
+  const handleLogin = () => {
+    const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+      REDIRECT_URI
+    )}&response_type=${RESPONSE_TYPE}&scope=user-read-playback-state`;
+    window.location = authUrl;
+  };
+
   return (
-    <div className="App">
-      <h1>Spotify Dance App</h1>
-      <button onClick={redirectToSpotifyAuth}>Connect Spotify</button>
+    <div>
+      <h1>Spotify Dancer</h1>
+      <button onClick={handleLogin}>Connect with Spotify</button>
     </div>
   );
 }
 
-
-<DancingCharacter tempo={tempo} energy={energy} />
+export default App;
